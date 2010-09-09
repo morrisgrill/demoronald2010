8d83aae087840aa605b0772264633f0e2cf12ed7
rnld1503 <rnld1503@a868f497-51af-cc1c-3f89-7d93472bdff0>
2010-09-09 03:19:21 +0000

    proyecto inicial

---
+++ trunk/demoSVN/Branches/1.0/MiProyectoUno/src/pe/com/demo/svn/Clase1.java
@@ -0,0 +1,14 @@
+package pe.com.demo.svn;
+
+public class Clase1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mi primera clase");
+
+	}
+
+}
