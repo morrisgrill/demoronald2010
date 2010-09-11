5716cb6dbcf3577a89a2a498c50ae644d35f276d
rnld1503 <rnld1503@a868f497-51af-cc1c-3f89-7d93472bdff0>
2010-09-11 19:09:38 +0000

    actualizacion del branch 1.0.2 - se resuelven los conflictos que existian al hacer megre sobre la version 1.0.1.

--- trunk/demoSVN/Trunk/1.0/core/src/pe/com/core/prueba/MainPueba.java
+++ trunk/demoSVN/Trunk/1.0/core/src/pe/com/core/prueba/MainPueba.java
@@ -1,13 +1,19 @@
 package pe.com.core.prueba;
 
 import pe.com.core.dao.EnvioMensajeDaoImp;
+import pe.com.core.dao.SeguridadDao;
+import pe.com.core.dao.SeguridadDaoImpl;
 import pe.com.core.service.EnvioMensajeService;
+import pe.com.core.service.SeguridadService;
 
 public class MainPueba {
 	
 	public static void main(String[] args){
 		EnvioMensajeService service = new EnvioMensajeService();
 		service.setEnvioMensajeDao(new EnvioMensajeDaoImp());
+		
+		SeguridadService seguridadService = new SeguridadService();
+		seguridadService.setDao(new SeguridadDaoImpl());
 		
 		/*invocando al servicio*/
 		service.enviarMensaje("Ronald ve a comer", "Maria");
@@ -16,6 +22,12 @@ public class MainPueba {
 		
 		service.enviarMensaje("Ronald ve a comer", "Ronald", " Maria", "Pedro" , "Renzo");
 		
+		
+		if(seguridadService.loguearusuario("admin", "admin")){
+			System.out.println("ingreso");
+		}else{
+			System.out.println("no ingreso");
+		}
 		
 	}
 
