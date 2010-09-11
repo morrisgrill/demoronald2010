959668b002bffb10f0f88014b7fc5e2c2664a7ca
rnld1503 <rnld1503@a868f497-51af-cc1c-3f89-7d93472bdff0>
2010-09-11 18:17:25 +0000

    primer Tag generado

---
+++ trunk/demoSVN/Tags/1.0/core/src/pe/com/core/prueba/MainPueba.java
@@ -0,0 +1,17 @@
+package pe.com.core.prueba;
+
+import pe.com.core.dao.EnvioMensajeDaoImp;
+import pe.com.core.service.EnvioMensajeService;
+
+public class MainPueba {
+	
+	public static void main(String[] args){
+		EnvioMensajeService service = new EnvioMensajeService();
+		service.setEnvioMensajeDao(new EnvioMensajeDaoImp());
+		
+		/*invocando al servicio*/
+		service.enviarMensaje("Ronald ve a comer", "Maria");
+		
+	}
+
+}
